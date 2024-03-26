ab714619f6b0c26e3934b0ed61eae22ae355f2d0
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-03-26 18:09:18 +0330

    Remake The Plugin.

---
+++ src/main/java/me/litedeforged/ldcore/LDCore.java
@@ -0,0 +1,17 @@
+package me.litedeforged.ldcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LDCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
