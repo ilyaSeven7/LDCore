f9acd64b799208581fcf084212edbc2c434b54b3
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-03-26 18:26:50 +0330

    Added Console Plugin Start Message!

--- src/main/java/me/litedeforged/ldcore/LDCore.java
+++ src/main/java/me/litedeforged/ldcore/LDCore.java
@@ -1,12 +1,16 @@
 package me.litedeforged.ldcore;
 
+import me.litedeforged.ldcore.message.Components;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LDCore extends JavaPlugin {
 
+    Components getter = new Components();
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getServer().getConsoleSender().sendMessage(getter.components("<red>Plugin Has Been Enabled!"));
 
     }
 
