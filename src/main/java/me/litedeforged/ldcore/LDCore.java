bfa3d448895d803f906a8dd95b28cd580a12989b
TheExile53 <fatehghollarata00@gmail.com>
2024-03-27 15:15:36 +0330

    Clean up DeathLogger ConfigFileLog

--- src/main/java/me/litedeforged/ldcore/LDCore.java
+++ src/main/java/me/litedeforged/ldcore/LDCore.java
@@ -1,7 +1,7 @@
 package me.litedeforged.ldcore;
 
 import me.litedeforged.ldcore.deathMessage.FileManager.ConfigManager;
-import me.litedeforged.ldcore.deathMessage.FileManager.Listeners.PlayerDeath;
+import me.litedeforged.ldcore.deathMessage.Listeners.PlayerDeath;
 import me.litedeforged.ldcore.message.Components;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
