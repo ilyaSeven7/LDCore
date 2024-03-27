f90f7f062c43c4ccaf98d1b7eb69ef84264708c3
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-03-27 23:51:20 +0330

    Comment For NickNameCheck Added.

--- src/main/java/me/litedeforged/ldcore/nicknamechanger/NickNameCheck.java
+++ src/main/java/me/litedeforged/ldcore/nicknamechanger/NickNameCheck.java
@@ -10,13 +10,13 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class NickNameCheck implements Listener {
 
-
+//When A Player Joining The Server Check Their Rank If They Have It Skip And If Not Reset Their Name Into The Default.
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Essentials ess = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
         Player player = event.getPlayer();
         assert ess != null;
-        if (!ess.getUser(player).getNick().equalsIgnoreCase(player.getName()) && !player.hasPermission("nickpermission.keepnickname") && LDCore.getInstance().getConfig().getConfigurationSection("nickName").getBoolean("toggle")) {
+        if (!ess.getUser(player).getNick().equalsIgnoreCase(player.getName()) && !player.hasPermission("nickpermission.keepnickname") && LDCore.getInstance().getConfig().getConfigurationSection("NickName").getBoolean("toggle")) {
             ess.getUser(player).setNickname(player.getName());
         }
 
