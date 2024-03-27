883ff40fa1b64e0cbafce6a47ad886d99c9b64f1
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-03-27 14:39:35 +0330

    Death Listener Class Created.

---
+++ src/main/java/me/litedeforged/ldcore/DeathMessage/Listeners.java
@@ -0,0 +1,13 @@
+package me.litedeforged.ldcore.DeathMessage;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class Listeners implements Listener {
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+
+
+    }
+}
