ae91c21620da5e635d7136908cd51ddb27469b12
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-03-31 00:23:59 +0330

    Added a System For Prevent Player From Using Command On Practice Lobby And None Crystal FFA Mode.

--- src/main/java/me/litedeforged/ldcore/practiceapi/commands/FFABack.java
+++ src/main/java/me/litedeforged/ldcore/practiceapi/commands/FFABack.java
@@ -3,6 +3,7 @@ package me.litedeforged.ldcore.practiceapi.commands;
 import ga.strikepractice.StrikePractice;
 import ga.strikepractice.api.StrikePracticeAPI;
 import me.litedeforged.ldcore.LDCore;
+import me.litedeforged.ldcore.message.Components;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -13,6 +14,8 @@ import org.jetbrains.annotations.NotNull;
 
 public class FFABack implements CommandExecutor {
 
+    Components getter = new Components();
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         StrikePracticeAPI strikePracticeAPI = StrikePractice.getAPI();
@@ -20,12 +23,17 @@ public class FFABack implements CommandExecutor {
             if (sender instanceof Player) {
                 Player player = ((Player) sender).getPlayer();
                 if (args.length == 0) {
+                    assert player != null;
+                    if (strikePracticeAPI.getFight(player) == null) {
+                        player.sendMessage(getter.components("<red>You Can Only Use This Command On FFa Crystal Mode"));
+                        return false;
+                    }
                     if (strikePracticeAPI.getFight(player).getArena().isFFA()) {
 
                         if (player.getLastDeathLocation() == null) return false;
 
                         if (LDCore.uuids.contains(player.getUniqueId())) {
-
+                            player.sendMessage(getter.components("<red>You Can't Use /Back For Now!"));
                             return false;
                         }
 
