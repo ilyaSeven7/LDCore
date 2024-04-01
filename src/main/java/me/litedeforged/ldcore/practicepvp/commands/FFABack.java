c65da0461eaae9c7e59663f699bb0a6c1ccbae7c
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-04-01 15:22:34 +0330

    Fully Fixed Back Commands.

--- src/main/java/me/litedeforged/ldcore/practicepvp/commands/FFABack.java
+++ src/main/java/me/litedeforged/ldcore/practicepvp/commands/FFABack.java
@@ -1,8 +1,7 @@
-package me.litedeforged.ldcore.practiceapi.commands;
+package me.litedeforged.ldcore.practicepvp.commands;
 
-import ga.strikepractice.StrikePractice;
-import ga.strikepractice.api.StrikePracticeAPI;
 import me.litedeforged.ldcore.LDCore;
+import me.litedeforged.ldcore.deathMessage.listeners.PlayerDeath;
 import me.litedeforged.ldcore.message.Components;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -13,36 +12,35 @@ import org.jetbrains.annotations.NotNull;
 public class FFABack implements CommandExecutor {
 
     Components getter = new Components();
+    PlayerDeath deathlocationgetter = new PlayerDeath();
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        StrikePracticeAPI strikePracticeAPI = StrikePractice.getAPI();
 
             if (sender instanceof Player) {
                 Player player = ((Player) sender).getPlayer();
                 if (args.length == 0) {
                     assert player != null;
-                    if (strikePracticeAPI.getFight(player) == null) {
+                    if (LDCore.strikePracticeAPI.getFight(player) == null) {
                         player.sendMessage(getter.components("<red>You Can Only Use This Command On FFa Crystal Mode"));
                         return true;
                     }
-                    if (strikePracticeAPI.getFight(player).getArena().getName().equalsIgnoreCase("crystalffa")) {
+                    if (LDCore.strikePracticeAPI.getFight(player).getArena().getName().equalsIgnoreCase("crystalffa")) {
 
-                        if (player.getLastDeathLocation() == null) return true;
 
-                        if (LDCore.uuids.contains(player.getUniqueId())) {
+                        if (LDCore.uuids.contains(player.getUniqueId()) || deathlocationgetter.getdeathlocation(player) == null) {
                             player.sendMessage(getter.components("<red>You Can't Use /Back For Now!"));
                             return true;
                         }
-
-
-                        player.teleport(player.getLastDeathLocation());
-
+                        getter.components("<green>You Teleported Back!");
+                        player.teleport(deathlocationgetter.getdeathlocation(player));
+                        deathlocationgetter.resetdeathlocation(player);
                         LDCore.uuids.add(player.getUniqueId());
 
                     }
                 }
             }
+
         return false;
     }
 }
