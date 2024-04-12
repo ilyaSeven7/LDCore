12bdabd72524389d85b6142a75f830698baf2ba4
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-04-13 00:03:37 +0330

    New Events Registered.

--- src/main/java/me/litedeforged/ldcore/LDCore.java
+++ src/main/java/me/litedeforged/ldcore/LDCore.java
@@ -1,25 +1,19 @@
 package me.litedeforged.ldcore;
 
-import me.litedeforged.ldcore.deathMessage.fileManager.ConfigManager;import me.litedeforged.ldcore.deathMessage.listeners.EntityDamagedByEntity;
-import me.litedeforged.ldcore.deathMessage.listeners.PlayerDeath;
+import me.litedeforged.ldcore.commands.*;
+import me.litedeforged.ldcore.deathMessage.fileManager.ConfigManager;
+import me.litedeforged.ldcore.listeners.*;
 import me.litedeforged.ldcore.message.Components;
-import me.litedeforged.ldcore.nicknamechanger.NickNameCheck;
+import me.litedeforged.ldcore.chatSystem.NickNameCheck;
 import me.litedeforged.ldcore.practicepvp.FFaArenaRollbackTimer;
-import me.litedeforged.ldcore.practicepvp.commands.ArenaRollbackTick;
-import me.litedeforged.ldcore.practicepvp.commands.FFALastDeathLocation;
-import me.litedeforged.ldcore.practicepvp.commands.ForceArenaRollback;
-import me.litedeforged.ldcore.practicepvp.commands.PartyLimit;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
 
 public final class LDCore extends JavaPlugin {
-
-    private static LDCore instance;
-
     public static LDCore getInstance() {return instance;}
-
+    private static LDCore instance;
     public static List<UUID> uuids = new ArrayList<>();
 
     FFaArenaRollbackTimer spRollBackPerTick = new FFaArenaRollbackTimer();
@@ -41,14 +35,18 @@ public final class LDCore extends JavaPlugin {
         getCommand("changerollbackspeed").setExecutor(new ArenaRollbackTick());
         getCommand("partylimit").setExecutor(new PartyLimit());
         getCommand("forcearenarollback").setExecutor(new ForceArenaRollback());
+        getCommand("clientdetector").setExecutor(new ClientDetector());
+
 
 
         Bukkit.getServer().getConsoleSender().sendMessage(getter.components("<green>Plugin Has Been Enabled!"));
         Bukkit.getPluginManager().registerEvents(new NickNameCheck(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerDeath(), this);
-//        Bukkit.getPluginManager().registerEvents(new PlayerJoin(), this);
-//        Bukkit.getPluginManager().registerEvents(new PlayerQuit(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerJoin(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerQuit(), this);
         Bukkit.getPluginManager().registerEvents(new EntityDamagedByEntity(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerStartParkour(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerEndParkour(), this);
         ConfigManager.setup();
     }
 
