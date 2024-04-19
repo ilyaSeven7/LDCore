b69b92537877aee7c180a0e196ee14923ee599ce
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-04-20 01:07:36 +0330

    Arg 1 Removed And Now Detect Automatically.

--- src/main/java/me/litedeforged/ldcore/commands/ArenaRollbackTick.java
+++ src/main/java/me/litedeforged/ldcore/commands/ArenaRollbackTick.java
@@ -22,26 +22,26 @@ public class ArenaRollbackTick implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if (sender instanceof Player player) {
-            if (args.length <= 1) {
-                player.sendMessage(mini.components("<red>Command Usage<dark_gray>: <gray>/<white>ChangeRollbackSpeed <dark_gray>(<white>ArenaName<dark_gray>) <dark_gray>(<white>ResetTick<dark_gray>)"));
+            if (args.length == 0) {
+                player.sendMessage(mini.components("<red>Command Usage<dark_gray>: <gray>/<white>ChangeRollbackSpeed <dark_gray>(<white>ResetTick<dark_gray>)"));
                 return false;
             }
-            if (args.length < 3) {
+            if (args.length < 2) {
 
-                if (spMethods.getArena(args[0]) == null) {
-                    player.sendMessage(mini.components("<red>Arena Not Exist!"));
+                if (spMethods.getFight(player) == null) {
+                    player.sendMessage(mini.components("<red>Arena Not Found!"));
                     return false;
                 }
 
-                if (spMethods.isInt(args[1]) == null) {
+                if (spMethods.isInt(args[0]) == null) {
                     player.sendMessage(mini.components("<red>You Need Import a Integer Value!"));
                     return false;
 
                 }else {
                     Bukkit.getScheduler().cancelTasks(LDCore.getInstance());
-                    player.sendMessage(mini.components("<aqua>ᴀʀᴇɴᴀ ʀᴏʟʟʙᴀᴄᴋ sᴘᴇᴇᴅ ᴄʜᴀɴɢᴇᴅ<dark_gray>: <red>" + spMethods.getConfigSec("PracticePvPSystem").getInt("FFaResetSpeed") / 60 + "<dark_gray> >>> <green>" + Integer.parseInt(args[1])));
-                    player.sendMessage(mini.components("<red>ᴀʀᴇɴᴀ ʀᴇsᴛᴀʀᴛ ᴇᴠᴇʀʏ <white>" + Integer.parseInt(args[1]) + "<red> ᴍɪɴᴜᴛᴇ!"));
-                    spMethods.getConfigSec("PracticePvPSystem").set("FFaResetSpeed", Integer.parseInt(args[1]) * 60);
+                    player.sendMessage(mini.components( spMethods.getFight(player).getArena().getName() + "<aqua> ᴀʀᴇɴᴀ ʀᴏʟʟʙᴀᴄᴋ sᴘᴇᴇᴅ ᴄʜᴀɴɢᴇᴅ<dark_gray>: <red>" + spMethods.getConfigSec("PracticePvPSystem").getInt("FFaResetSpeed") / 60 + "<dark_gray> » <green>" + Integer.parseInt(args[0])));
+                    player.sendMessage(mini.components(spMethods.getFight(player).getArena().getName() + "<red> ᴀʀᴇɴᴀ ʀᴇsᴛᴀʀᴛ ᴇᴠᴇʀʏ <white>" + Integer.parseInt(args[0]) + "<red> ᴍɪɴᴜᴛᴇ!"));
+                    spMethods.getConfigSec("PracticePvPSystem").set("FFaResetSpeed", Integer.parseInt(args[0]) * 60);
                     LDCore.getInstance().saveConfig();
                     LDCore.getInstance().reloadConfig();
                     spRollBackPerTick.resetFFAPerTick();
